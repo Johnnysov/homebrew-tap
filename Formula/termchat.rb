e8bc50d8bde0c3481ce8df3612daeea9f5c5b238
Johnny Sovacool <johnnysov@icloud.com>
2026-07-31 19:16:16 -0500

    Create termchat.rb

---
+++ Formula/termchat.rb
@@ -0,0 +1,17 @@
+class Termchat < Formula
+  desc "Simple TCP terminal chat server and client"
+  homepage "https://github.com/Johnnysov/termchat"
+  url "https://github.com/Johnnysov/termchat/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    bin.install "termchat.js" => "termchat"
+  end
+
+  test do
+    system "#{bin}/termchat", "-h"
+  end
+end
